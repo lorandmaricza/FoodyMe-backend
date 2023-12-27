2069a95d485ff900dcae96249390b15499dff5b2
Lorand Maricza <maricza.lorand@gmail.com>
2023-12-27 12:14:41 +0100

    add express-session

--- index.ts
+++ index.ts
@@ -10,16 +10,29 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const session = require("express-session");
+require("dotenv").config();
 const port = 3001;
+const day = 1000 * 60 * 60 * 24;
 
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(session({
-    secret: 'your-secret-key',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false } //https -> true
+    cookie: {
+        secure: false,
+        maxAge: day
+    } //secure should be true in production
 }));
+
+app.get("/session", (req: Request, res: Response) => {
+    if (req.session.user) {
+        res.send({ loggedIn: true, user: req.session.user });
+    } else {
+        res.send({ loggedIn: false, message: "Not logged in" });
+    }
+});
 
 app.post("/user/login", (req: Request, res: Response) => {
     login(req, res);
